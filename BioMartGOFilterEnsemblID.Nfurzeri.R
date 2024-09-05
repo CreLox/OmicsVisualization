0dae8c2d0b83669abadbd047d6489731be6eea41
寒江雪 <18239347+CreLox@users.noreply.github.com>
2024-09-05 19:02:16 +0200

    Add files via upload

--- BioMartGOFilterEnsemblID.Nfurzeri.R
+++ BioMartGOFilterEnsemblID.Nfurzeri.R
@@ -1,5 +1,6 @@
 BioMartGOFilterEnsemblID.Nfurzeri <- function(GO.CSV, CombineFruitFlyHomology = TRUE, CombineHumanHomology = TRUE, CombineNematodeHomology = TRUE, CombineXenopusHomology = TRUE, CombineZebrafishHomology = TRUE) {
   suppressPackageStartupMessages(library("biomaRt"))
+  biomartCacheClear()
   
   KillifishTable <-
     getBM(attributes = c("ensembl_gene_id", "external_gene_name"),
