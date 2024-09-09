8083446c00579da24f5ce3163f504993b20c0343
寒江雪 <18239347+CreLox@users.noreply.github.com>
2024-09-09 15:05:38 +0200

    Add files via upload

---
+++ FindUniqueGenes.EnsemblID.R
@@ -0,0 +1,26 @@
+FindUniqueGenes.EnsemblID <- function(TargetSpecies = "nfurzeri",
+                                      CheckHomologySpecies = c("drerio", "olatipes", "ssalar")) {
+  
+  suppressPackageStartupMessages(library("biomaRt"))
+  suppressPackageStartupMessages(library("retry"))
+  biomartCacheClear()
+  
+  UniqueSet <- NULL
+  for (HomologySpecies in CheckHomologySpecies) {
+    retry({
+      Table <-
+      getBM(attributes = c("ensembl_gene_id", "external_gene_name"),
+            filters = paste("with", HomologySpecies, "homolog", sep = "_"), values = FALSE,
+            mart = useEnsembl(biomart = "ensembl", dataset = paste(TargetSpecies, "gene_ensembl", sep = "_")))
+    }, when = "Error", silent = TRUE)
+    row.names(Table) <- NULL
+    if (is.null(UniqueSet)) {
+      UniqueSet <- Table[, "ensembl_gene_id"]
+    }
+    else {
+      UniqueSet <- intersect(UniqueSet, Table[, "ensembl_gene_id"])
+    }
+  }
+  
+  return(UniqueSet)
+}
