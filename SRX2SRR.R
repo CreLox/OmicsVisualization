ba88827b720056d8fcf0c25f66a84b8b110cc77a
寒江雪 <18239347+CreLox@users.noreply.github.com>
2024-10-18 14:18:33 +0200

    Add files via upload

--- SRX2SRR.R
+++ SRX2SRR.R
@@ -1,18 +1,20 @@
 SRX2SRR <- function(SRXSheetFilePath, SRXColumnName = "SRX") {
-  library("readxl")
-  library("rvest")
-  library("pracma")
+  suppressPackageStartupMessages(library("readxl"))
+  suppressPackageStartupMessages(library("rvest"))
+  suppressPackageStartupMessages(library("pracma"))
   
   SRXSheetContent <- read_excel(SRXSheetFilePath)
   SRXVector <- SRXSheetContent[[SRXColumnName]]
-  fprintf("   Run#   \tFormat\n-----------\t------\n")
+  fprintf("Experiment#\t   Run#   \tFormat\n-----------\t-----------\t------\n")
   for (i in 1 : length(SRXVector)) {
     if (is.na(SRXVector[i])) {
-    	  fprintf("\n")
-    	}
-    	else {
+      fprintf("\n")
+    }
+    else {
       url <- paste('https://www.ncbi.nlm.nih.gov/sra/', SRXVector[i], sep = "")
       SRXPage <- read_html(url)
+      
+      fprintf(paste(SRXVector[i], "\t", sep = ""))
       
       RemoveAfterRunNumber <- sub("</a></td>\n<td align=\"right\">.*", "", SRXPage)
       SRRNumber <- sub(".*\">SRR", "", RemoveAfterRunNumber)
