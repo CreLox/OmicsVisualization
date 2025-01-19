e272600c6c288c8bd06f989758ca11f367099c1b
寒江雪 <18239347+CreLox@users.noreply.github.com>
2025-01-19 01:33:14 +0100

    Add files via upload

--- volcano.ma.R
+++ volcano.ma.R
@@ -1,16 +1,23 @@
-volcano.ma <- function(Data, PlotType = "ma", HighlightIDs = NA, GeneNameColumnName = "gene_name", IDColumnName = "ensembl_gene_id", log2FoldChangeColumnName = "log2FoldChange", abslog2FoldChangeThreshold = 1, abslog2FoldChangeLimit = 3, baseMeanColumnName = "baseMean", log2baseMeanLowerLimit = 0, log2baseMeanUpperLimit = NA, AdjustedPValueColumnName = "padj", SignificanceThreshold = 0.01, negativelog10AdjustedPValueLimit = 15, LineWidth = 0.25, Alpha = 1, NSAlpha = 0.1, UpColor = "#FFD300", DownColor = "#0087BD", HighlightColor = "#C40233", HighlightSize = 2.5, log2FoldChangeLabel = bquote(log[2](Escape/Diapause)), log2FoldChangeTickDistance = 1, log10AdjustedPValueTickDistance = 5) {
+volcano.ma <- function(Data, PlotType = "ma", HighlightIDs = NA, GeneNameColumnName = "gene_name", IDColumnName = "ensembl_gene_id", log2FoldChangeColumnName = "log2FoldChange", Invertlog2FoldChange = FALSE, abslog2FoldChangeThreshold = 1, abslog2FoldChangeLimit = 3, baseMeanColumnName = "baseMean", log2baseMeanLowerLimit = 0, log2baseMeanUpperLimit = NA, AdjustedPValueColumnName = "padj", SignificanceThreshold = 0.01, negativelog10AdjustedPValueLimit = 15, LineWidth = 0.25, Alpha = 1, NSAlpha = 0.1, UpColor = "#FFD300", DownColor = "#0087BD", HighlightColor = "#C40233", HighlightSize = 2.5, log2FoldChangeLabel = bquote(log[2](Escape/Diapause)), log2FoldChangeTickDistance = 1, log10AdjustedPValueTickDistance = 5) {
   suppressPackageStartupMessages(library("ggplot2"))
   
+  # Initialization
   Data <- as.data.frame(Data)
+  names(Data)[names(Data) == log2FoldChangeColumnName] <- "log2FoldChange"
+  names(Data)[names(Data) == GeneNameColumnName] <- "gene_name"
+  if (Invertlog2FoldChange) {
+    Data[, "log2FoldChange"] <- -Data[, "log2FoldChange"]
+  }
+  
   # Categorize each gene based on its log2FoldChange and AdjustedPValue and assemble the data frame
   Category = rep("ns", nrow(Data))
   for (i in 1 : nrow(Data)) {
-    if ((Data[i, log2FoldChangeColumnName] >= abslog2FoldChangeThreshold) &
+    if ((Data[i, "log2FoldChange"] >= abslog2FoldChangeThreshold) &
         (!is.na(Data[i, AdjustedPValueColumnName])) &
         (Data[i, AdjustedPValueColumnName] < SignificanceThreshold)) {
       Category[i] = "up"
     }
-    if ((Data[i, log2FoldChangeColumnName] <= -abslog2FoldChangeThreshold) &
+    if ((Data[i, "log2FoldChange"] <= -abslog2FoldChangeThreshold) &
         (!is.na(Data[i, AdjustedPValueColumnName])) &
         (Data[i, AdjustedPValueColumnName] < SignificanceThreshold)) {
       Category[i] = "down"
@@ -22,8 +29,6 @@ volcano.ma <- function(Data, PlotType = "ma", HighlightIDs = NA, GeneNameColumnN
     log2baseMean <- log2(Data[, baseMeanColumnName])
     Data <- cbind(Data, log2baseMean)
   }
-  names(Data)[names(Data) == log2FoldChangeColumnName] <- "log2FoldChange"
-  names(Data)[names(Data) == GeneNameColumnName] <- "gene_name"
   
   # Preprocessing
   Data <- Data[!is.na(Data[, AdjustedPValueColumnName]),]
