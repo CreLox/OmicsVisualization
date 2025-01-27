87a306bf875d503e08f2fa6b05493db472ba6b25
寒江雪 <18239347+CreLox@users.noreply.github.com>
2025-01-27 17:42:32 +0100

    Add files via upload

--- write.gmt.R
+++ write.gmt.R
@@ -2,17 +2,28 @@ write.gmt <- function(GOList,
                       OutputFilePath = "custom.gmt") {
   
   suppressPackageStartupMessages(library("ribiosUtils"))
+  suppressPackageStartupMessages(library("GO.db"))
+  GOTermList <- as.list(GOTERM)
   InvertedGOList <- invertList(GOList)
   
   # if a file of this path already exists, this overwrites it with an empty file
   file.create(OutputFilePath)
   
   for (i in 1 : length(InvertedGOList)) {
-    Line <- names(InvertedGOList[i])
+    GOID <- names(InvertedGOList[i])
+    if (!is.null(GOTermList[[GOID]])) {
+      Description <- Term(GOTermList[[GOID]])
+    }
+    else {
+    	  Description <- "Null"
+    }
+    Line <- paste(GOID, Description, sep = "\t")
+    
     Elements <- InvertedGOList[[i]]
     for (j in 1 : length(Elements)) {
       Line <- paste(Line, Elements[j], sep = "\t")
     }
+    
     write(Line, file = OutputFilePath, append = TRUE)
   }
 }
