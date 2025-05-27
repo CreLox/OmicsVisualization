fb797a1511b601a30601b3bd3d90ae1e2846dba0
寒江雪 <18239347+CreLox@users.noreply.github.com>
2025-05-28 01:34:23 +0200

    Add files via upload

--- plotPCA.r
+++ plotPCA.r
@@ -1,8 +1,11 @@
 plotPCA <- function(OmicsExcelDataFilePath = "LFQ_intensities_Temperature.xlsx",
                     SampleColumns = 2 : 29,
                     GroupTags = c(rep("DII (arrested at 24 °C)", 6), rep("DII (persistent at 28.5 °C)", 11), rep("Development resumption", 11)),
-                    PointSize = 2,
-                    FillColorPalette = c("#FFD300", "#FFFFFF", "#0087BD")) {
+                    PointSize = 2.5,
+                    xlim = c(-70, 100),
+                    ylim = c(-50, 120),
+                    FillColorPalette = c("#FFD300", "#FFFFFF", "#0087BD"),
+                    HideAxis = FALSE) {
 
   suppressPackageStartupMessages(library("readxl"))
   suppressPackageStartupMessages(library("ggplot2"))
@@ -15,9 +18,13 @@ plotPCA <- function(OmicsExcelDataFilePath = "LFQ_intensities_Temperature.xlsx",
 
   Plot <- ggplot(data = as.data.frame(PCA$x), aes(x = PC1, y = PC2, fill= GroupTags)) +
           geom_point(color = "black", pch = 21, size = PointSize) +
-          theme_bw() + theme(panel.grid.minor = element_blank(), panel.grid.major = element_blank(), legend.title = element_blank(), legend.position = "bottom", axis.text.x = element_blank(), axis.ticks.x = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank(), legend.box.spacing = unit(0, "pt"), legend.text = element_text(margin = margin(l = unit(0, "pt")))) +
+          theme_bw() + theme(panel.grid.minor = element_blank(), panel.grid.major = element_blank(), legend.title = element_blank(), legend.position = "bottom", legend.box.spacing = unit(0, "pt"), legend.text = element_text(margin = margin(l = unit(0, "pt")))) +
           scale_fill_manual(values = FillColorPalette) +
+          coord_fixed(ratio = 1, xlim = xlim, ylim = ylim) +
           xlab(sprintf("PC1 (%.1f%%)", PC1VarPercent * 100)) + ylab(sprintf("PC2 (%.1f%%)", PC2VarPercent * 100))
+  if (HideAxis) {
+    Plot <- Plot + theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank())
+  }
   
   return(Plot)
 }
