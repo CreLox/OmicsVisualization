4792b4bcad48a8e179bbc484a587661a08ff0be3
寒江雪 <18239347+CreLox@users.noreply.github.com>
2025-08-05 16:58:49 +0200

    Add files via upload

--- write.gmt.EC.r
+++ write.gmt.EC.r
@@ -1,7 +1,7 @@
 write.gmt.EC <- function(ECIndexFilePath = "EC.20250802.des",
                          organism_id = "105023", # Nothobranchius furzeri: 105023
                          To = "Ensembl",
-                         AppendGODescription = FALSE,
+                         AppendDescription = FALSE,
                          LowCountThreshold = 5,
                          IgnoreLevel = c(0, 1), # Example: "EC1": lv 0, "EC1.14": lv 1, "EC1.14.11": lv 2
                          GMTFilePath = "EC.N.furzeri.gmt") {
@@ -20,10 +20,10 @@ write.gmt.EC <- function(ECIndexFilePath = "EC.20250802.des",
     ECNumber <- unlist(strsplit(ECIndex[i, 1], split = "EC "))[2]
     if (!(str_count(ECNumber, "\\.") %in% IgnoreLevel)) {
       system(
-        paste0("curl -o write.gmt.EC.temp ",
-               "'https://rest.uniprot.org/uniprotkb/stream?download=true&fields=accession&format=tsv&query=((ec:", ECNumber,
-               ")+AND+(organism_id:", organism_id,
-               "))'")
+             paste0("curl -o write.gmt.EC.temp ",
+                    "'https://rest.uniprot.org/uniprotkb/stream?download=true&fields=accession&format=tsv&query=((ec:", ECNumber,
+                    ")+AND+(organism_id:", organism_id,
+                    "))'")
       )
       curl.Results <- read.table("write.gmt.EC.temp", header = TRUE)
       EnsemblIDs <- unique(UniProtKBAC2EnsemblID(paste0(curl.Results[, "Entry"], collapse = ","), To = To)[, 2])
@@ -31,7 +31,7 @@ write.gmt.EC <- function(ECIndexFilePath = "EC.20250802.des",
       if (length(EnsemblIDs) >= LowCountThreshold) {
         # may also filter out NA/""/NULL/character(0) because length(NA) == length("") == 1
         # and length(NULL) == length(character(0)) == 0
-        if (AppendGODescription) {
+        if (AppendDescription) {
           Line <- paste0(ECIndex[i,], collapse = "\t")
         }
         else {
