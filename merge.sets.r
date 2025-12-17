a31f066b86c607a8287f2e39a80e71a838cb06e3
寒江雪 <18239347+CreLox@users.noreply.github.com>
2025-12-17 01:18:18 +0100

    Add files via upload

--- merge.sets.r
+++ merge.sets.r
@@ -1,15 +1,11 @@
-merge.sets <- function(set.a, set.b = c(), exception.set = c()) {
+merge.sets <- function(set.a, set.b = c(), boundary.set = NULL, exception.set = c()) {
   merged.set <- unique(c(set.a, set.b))
   
-  i <- 1
-  while (i <= length(merged.set)) {
-    if (merged.set[i] %in% exception.set) {
-      merged.set <- merged.set[-i]
-    }
-    else {
-      i <- i + 1
-    }
+  if (length(boundary.set) > 0) {
+    merged.set <- merged.set[merged.set %in% boundary.set]
   }
+  
+  merged.set <- merged.set[!(merged.set %in% exception.set)]
   
   return(merged.set)
 }
